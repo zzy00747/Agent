6939756c8a83a0d87ab69507266520bd9562b4a0
zzy00747 <1285445576@qq.com>
2026-01-28 09:18:48 +0800

    add auto format config files

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      '*.min.js',
+      '*.min.ts',
+      'logs/**',
+      '.git/**',
+      '.vscode/**',
+      'coverage/**',
+      '*.config.js',
+      '*.config.cjs',
+      'database.sqlite',
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'prefer-template': 'error',
+      'eqeqeq': ['error', 'always'],
+      'no-unused-expressions': 'error',
+      'no-empty': ['error', { allowEmptyCatch: true }],
+    },
+  },
+];
